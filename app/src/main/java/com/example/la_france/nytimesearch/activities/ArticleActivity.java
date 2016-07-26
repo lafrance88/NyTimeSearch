516d9c9f4f5e886299f3702ee9f88f97a8150956
La-France Jean Robert <lafrancejeanrobert@gmail.com>
2016-07-26 16:15:07 -0700

    Upload my app NyTimeSearch

---
+++ app/src/main/java/com/example/la_france/nytimesearch/activities/ArticleActivity.java
@@ -0,0 +1,40 @@
+package com.example.la_france.nytimesearch.activities;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.example.la_france.nytimesearch.R;
+import com.example.la_france.nytimesearch.R;
+
+
+
+
+
+public class ArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+      String url = getIntent().getStringExtra("url");
+
+        WebView webView = (WebView) findViewById(R.id.wvArticle);
+        webView.loadUrl(url);
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                view.loadUrl(url);
+                return true;
+
+           }
+
+        });
+    }
+
+}
